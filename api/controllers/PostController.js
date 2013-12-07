e41dc101742b30d76b56fd974a5705683915c0be
shrikanth <shrikanth@shrikanth.(none)>
2013-12-07 23:36:56 +0530

    initial commit

---
+++ api/controllers/PostController.js
@@ -0,0 +1,76 @@
+/**
+ * PostController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+ module.exports = {
+
+
+
+
+  /**
+   * Overrides for the settings in `config/controllers.js`
+   * (specific to PostController)
+   */
+   _config: {},
+   new: function(req,res){
+   	console.log('New action called');
+   	return res.view('posts/new',{
+   		posts: [{title: 'Title of the post'}]
+   	});
+
+   },
+   create: function(req,res){
+   	console.log('Create action called'+req.param('title') );
+   	Post.create({
+   		title: req.param('title'),
+   		description: req.param('description')
+   	}).done(function(err, post){
+   		if (err) {
+   			return console.log(err);
+
+  // The User was created successfully!
+}else {
+	console.log("Post created:", post);
+	posts = Post.find();
+	console.log(posts);
+	return res.view('posts/index',{posts: posts});
+}
+} );
+
+   },
+   show: function(req,res){
+
+   },
+   edit: function(req,res){
+
+   },
+   update: function(req,res){
+
+   },
+   destroy: function(req,res){
+
+   },
+   index: function(req,res){
+   	console.log("index");
+   	posts = Post.find({}, function(err, posts) {
+   		console.log("TITLE:"+posts[0].title);
+   		return res.view('posts/index',{ posts: posts});
+   	});
+   	
+   },
+
+
+};
---
+++ api/models/Post.js
@@ -0,0 +1,23 @@
+/**
+ * Post
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+
+ module.exports = {
+
+
+
+ 	adapter: 'postgres',
+ 	tableName: 'posts',
+ 	attributes: {
+
+ 		title: 'string',
+ 		description: 'string'
+
+ 	}
+
+ };
